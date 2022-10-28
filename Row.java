59d13e2dd9bf2495ef4b32a444f19b843294d220
spookywoodie <116889586+spookywoodie@users.noreply.github.com>
2022-10-28 20:57:56 +0800

    Add files via upload

---
+++ Row.java
@@ -0,0 +1,20 @@
+
+
+/**
+ *
+ * @author Metch
+ */
+public class Row {
+    private static int row;
+    public static int getRow(){
+        return row;
+    }
+    public static void setRow(){
+        row++;
+    }
+    public static void setRow( int temp){
+        row= temp;
+    }
+
+
+}
